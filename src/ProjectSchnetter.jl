d2953e1efbe7606b857f896c91dc43d1a8d0a356
Christopher Waas <43119886+chriswaas@users.noreply.github.com>
2018-09-26 16:30:06 -0400

    added 3D case

--- src/ProjectSchnetter.jl
+++ src/ProjectSchnetter.jl
@@ -1,7 +1,7 @@
 module ProjectSchnetter
-#using Plots
-export picture
-function picture(o::Number, gamma::Number, x_0::Number, y_0::Number, v_x::Number, v_y::Number, deltaT::Number) # gamma =/=0
+
+export picture2d
+function picture2d(o::Number, gamma::Number, x_0::Number, y_0::Number, v_x::Number, v_y::Number, deltaT::Number) # gamma =/=0
   omega = sqrt(o^2-gamma^2/4)
   A = sqrt(x_0^2+v_x^2/omega^2)
   B = sqrt(y_0^2+v_y^2/omega^2)
@@ -16,12 +16,29 @@ function picture(o::Number, gamma::Number, x_0::Number, y_0::Number, v_x::Number
   end
   return x,y
 end
-#plot(x,y)
 
-export bla
-function bla(x)
-  return x
+export picture3d
+function picture3d(o::Number, gamma::Number, x_0::Number, y_0::Number,z_0::Number, v_x::Number, v_y::Number, v_z::Number, deltaT::Number) # gamma =/=0
+  omega = sqrt(o^2-gamma^2/4)
+  A = sqrt(x_0^2+v_x^2/omega^2)
+  B = sqrt(y_0^2+v_y^2/omega^2)
+  C = sqrt(z_0^2+v_z^2/omega^2)
+  len = max(trunc(Int, log(A)*20/(gamma*deltaT)), trunc(Int, log(B)*20/(gamma*deltaT)),trunc(Int, log(C)*20/(gamma*deltaT)))
+  x = zeros(Float64, len, 1)
+  y = zeros(Float64, len, 1)
+  z = zeros(Float64, len, 1)
+  t = 0
+  for i = 1:length(x)
+    x[i] = A*exp(-gamma/2*t)*cos(omega*t+acos(x_0/A))
+    y[i] = B*exp(-gamma/2*t)*cos(omega*t+acos(y_0/B))
+    z[i] = C*exp(-gamma/2*t)*cos(omega*t+acos(z_0/C))
+    t += deltaT
+  end
+  return x,y,z
 end
+
+
+
 
 end # module
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,19 +1,26 @@
 using ProjectSchnetter
 using Plots
-Plots.scalefontsizes(0.5)
+Plots.scalefontsizes(0.8)
 using Distributions
 import Distributions: Uniform
 plotly()
-for i in 1:5
+for i = 1:4
   o = rand(17:47) #rand(Uniform(5,25))
   gamma = rand(0.5:6) #rand(Uniform(0.5,4))
   x = rand(-20:20) # rand(Uniform(-50,50))
   y = rand(-20:20) #rand(Uniform(-50,50))
+  z = rand(-20:20) #rand(Uniform(-50,50))
   vx = rand(-100:100) #rand(Uniform(-60,60))
   vy = rand(-100:100) #rand(Uniform(-60,60))
-  p = picture(o,gamma,x,y,vx,vy,0.005)
-  t = string(o, ", ",gamma, ", ",x, ", ",y, ", ",vx, ", ",vy)
-  plot(p, title = t)
+  vz = rand(-100:100) #rand(Uniform(-60,60))
+  if i <=2
+    p = picture2d(o,gamma,x,y,vx,vy,0.005)
+    t = string("omega = ",o, ", gamma = ",gamma, ", x_0 = ",x, ", y_0 = ",y, ", vx_0 = ",vx, ", vy_0 = ",vy)
+    plot(p, title = t)
+  else
+    p = picture3d(o,gamma,x,y,z,vx,vy,vz,0.005)
+    t = string("omega = ",o, ", gamma = ",gamma, ", x_0 = ",x, ", y_0 = ",y, ", z = ",z, ", vx_0 = ",vx, ", vy_0 = ",vy, ", vz_0 = ",vz)
+    plot3d(p, title = t)
+  end
   gui()
 end
-sleep(10)
